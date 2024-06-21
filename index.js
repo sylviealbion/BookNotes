da9e39da401f20e30df4f88b92c131488a5dc7db
sylviealbion <121210067+sylviealbion@users.noreply.github.com>
2024-06-21 22:44:07 +0800

    Can Search for books and display as drop down. Cant select yet, and drop down wont hide yet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
 import pg from "pg";
 
@@ -7,6 +8,17 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+let booksArray=[];
+
+function Book(coverID, title, authorName, key, authorKey){
+    this.coverID = coverID;
+    this.title = title;
+    this.authorName = authorName;
+    this.key = key;
+    this.authorKey =authorKey;
+}
+
 
 const db = new pg.Client({
     user: "postgres",
@@ -29,9 +41,37 @@ app.get("/view", async (req,res)=>{
 
 //go to add and entry page
 app.get("/add", async (req,res)=>{
-    res.render("entry.ejs");
+    console.log(booksArray.length);
+    res.render("entry.ejs",{books: booksArray});
 });
 
+app.post("/search", async (req,res)=>{
+    booksArray=[];
+    try {       
+        const result = await axios.get("https://openlibrary.org/search.json",{
+            params:{
+                q: req.body.search,
+                limit:10
+            }
+        });
+        let resultArray = result.data.docs;
+        resultArray.forEach(async item => {
+            let book = new Book(
+                item.cover_i,
+                item.title,
+                item.author_name,
+                item.key.slice(7),
+                item.author_key
+            );
+            booksArray.push(book);
+        });
+         console.log(booksArray);
+        res.redirect("/add");
+    } catch (error) {
+        console.log(error);
+        res.redirect("/add"); 
+    }    
+});
 
 //add new data
 app.post("/entry", async (req,res)=>{
@@ -40,12 +80,12 @@ app.post("/entry", async (req,res)=>{
 });
 
 //edit data
-app.post("/", async (req,res)=>{
+app.patch("/", async (req,res)=>{
     
 });
 
 //delete data
-app.post("/", async (req,res)=>{
+app.delete("/", async (req,res)=>{
     
 });
 
